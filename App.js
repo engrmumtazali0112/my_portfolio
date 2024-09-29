5f1e678b1e74ca77cf64e8809a7dad2843727ab7
Abuzar Khan <149166915+Abuzar-Khan2023@users.noreply.github.com>
2024-09-29 21:20:42 +0000

    set files

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React, {useState} from "react";
 
 import './index.css';
-import './Components/Navbar'
-import Home from "./Routes/Home.js"
-import About from "./Routes/About.js"
-import Project from "./Routes/Project.js"
-import Contact from "./Routes/Contact.js"
-import Gallery from "./Routes/Gallery.js"
-import DownloadCv from "./Routes/DownloadCv.js"
+import './Portfolio/src/Components/Navbar.js'
+import Home from "./Portfolio/src/Routes/Home.js"
+import About from "./Portfolio/src/Routes/About.js"
+import Project from "./Portfolio/src/Routes/Project.js"
+import Contact from "./Portfolio/src/Routes/Contact.js"
+import Gallery from "./Portfolio/src/Routes/Gallery.js"
+import DownloadCv from "./Portfolio/src/Routes/DownloadCv.js"
 import { Route, Routes } from "react-router-dom";
 
 function App() {
