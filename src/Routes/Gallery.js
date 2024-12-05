5f9cb4f8380d720491c034446215c12945dcbefe
Mumtaz Ali <156393630+engrmumtazali0112@users.noreply.github.com>
2024-12-06 01:11:04 +0500

    Add files via upload

---
+++ src/Routes/Gallery.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+import FramedPicture from '../Components/FramedPicture';
+
+const Gallery = () => {
+  return (
+    <div>
+      <Navbar/>
+      <FramedPicture/>
+      <Footer/>
+    </div>
+  );
+};
+
+export default Gallery;
