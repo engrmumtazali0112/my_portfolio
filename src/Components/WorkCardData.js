c9c60472e2daebe922f7754564371d5c54236e9d
Mumtaz Ali <engrmumtazali01@gmail.com>
2025-05-22 10:54:46 +0500

    feat: update project portfolio data and descriptions

--- src/Components/WorkCardData.js
+++ src/Components/WorkCardData.js
@@ -1,50 +1,171 @@
-import pro1 from "../assets/pro1.png"
-import pro2 from "../assets/pro2.png"
-import pro3 from "../assets/pro3.png"
-import pro4 from "../assets/pro4.jpg"
-import pro5 from "../assets/pro5.jpeg"
+import p1blogwebsite from "../assets/p1blogwebsite.PNG"
+
+import p4handgesture from "../assets/p4handgesture.gif"
+import p5objdetection from "../assets/p5objdetection.gif"
+import p6PersonalityPredictionSystem from "../assets/p6PersonalityPredictionSystem.gif"
+import p7RubustMultipleChoisequiz from "../assets/p7RubustMultipleChoisequiz.gif"
+import p11ModernMusicPlayer from "../assets/p11ModernMusicPlayer.gif";
+import p12DocumentPlagiarismDetector from "../assets/p12DocumentPlagiarismDetector.gif";
+import p8HangmanAdventureDjango from "../assets/p8HangmanAdventureDjango.gif"
+import p9WorldBankPopulationData from "../assets/p9WorldBankPopulationData.JPG";
+import p2ecommerce from "../assets/p2ecommerce.png"
+import p3studyhub from "../assets/p3studyhub.PNG"
+import p14python_scripting_amazon_scraping from "../assets/p14python_scripting_amazon_scraping.gif";
+import p10TitanicDatasetAnalysisReportimages from "../assets/p10TitanicDatasetAnalysisReportimages.jpg";
+import p13VoiceAssistan from "../assets/p13VoiceAssistan.PNG";
+import p14JewelryShopStore from "../assets/p14JewelryShopStore.gif";
 
 
 const ProjectCardData = [
     {
-        imgsrc: pro1,
-        title: "EarningInk",
-        text: "Earningink isn’t just another blogging platform. It’s a community where your voice matters, and your content can make a real impact. Whether you’re a seasoned writer or just starting, we provide you with the tools and opportunities to grow and monetize your passion.",
-        view: "https://earningink.netlify.app/",
-        sourse: "https://github.com/Abuzar-Khan2023/EarningInk"
+        imgsrc: p14JewelryShopStore,
+        title: "Jewelry Shop Management System",
+        text: "A comprehensive web-based application designed to manage and streamline the operations of a jewelry shop. The system includes features for inventory management, order processing, customer management, and sales tracking. Developed using Django, the project provides an intuitive interface for both customers and shop staff, enabling efficient management of the shop's daily operations.",
+        view: "https://github.com/engrmumtazali0112/Jewelry-Shop---Django-E-commerce",
+        source: "https://github.com/engrmumtazali0112/Jewelry-Shop---Django-E-commerce"
+    },
+
+
+    {
+        imgsrc: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExaGx2b3ZkdHFjdnFmb2ZkdGFkdGVkdGVkdGVkdGVkdGVkdGVkdGVkdGVkZQ/giphy.gif",
+        title: "Smart Hostel Management System",
+        text: "A full-stack hostel management system built with Django and Bootstrap. It streamlines room allocation, mess management, payments, complaints, and more. Key features include:\n\n- Room & Mess Management\n- Stripe Payment Integration\n- Complaint Handling & Visitor Management\n- Biometric Attendance Tracking\n- Automated Notifications & Admin Dashboard\n- Student Portal & Digital Notice Board\n- Multi-database Support (SQLite & PostgreSQL)",
+        view: "https://github.com/engrmumtazali0112/SmartHostelManagmentSystem_FYP",
+        source: "https://github.com/engrmumtazali0112/SmartHostelManagmentSystem_FYP"
     },
 
     {
-        imgsrc: pro2, 
-        title: "Word Pieas",
-        text: "World Peas is E-commerce website where people can buy every type of Fresh Vagitables with free delivery inside there city",
-        view: "https://abuzar-khan2023.github.io/World-Peas/",
-        sourse: "https://github.com/Abuzar-Khan2023/World-Peas"
+        imgsrc: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmZrcmFvc2JvODlwaHRmYm9mZnJ5aXNlbGc2MHkyN2o1NDI3N3M0ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7aCScwdMAohPCq76/giphy.gif",
+        title: "Interactive Quiz Game",
+        text: "A robust command-line and web-based quiz game with PostgreSQL database integration, designed to deliver engaging multiple-choice questions from various categories with comprehensive data persistence and analytics.",
+        view: "https://github.com/engrmumtazali0112/Robust-Multiple-Choice-Quiz-Game",
+        source: "https://github.com/engrmumtazali0112/Robust-Multiple-Choice-Quiz-Game",
     },
 
     {
-        imgsrc: pro3,
-        title: "Grocery Managmnet System",
-        text: "This is C++ based online shoping management system where have spacific login and sigin up for both user and admin. Admin can add, drop, or update product where User can select products from the list shown and can generate there invoice.",
-        view: "https://github.com/Abuzar-Khan2023/-Online-Shopping-System-Grocery-System-",
-        sourse: "https://github.com/Abuzar-Khan2023/-Online-Shopping-System-Grocery-System-"
+        imgsrc: p14python_scripting_amazon_scraping,
+        title: "Amazon Scraper",
+        text: "Amazon_scraper is a Python-based project that scrapes Amazon product data such as titles, prices, reviews, and images. It utilizes Flask for the backend and BeautifulSoup for scraping. The project also includes a frontend for querying data. Features include:\n- Scraping product data from Amazon using search queries.\n- Saving scraped data in JSON and CSV formats.\n- A Flask API to serve the scraped data.\n- A React-based frontend for querying and displaying data.",
+        view: "https://amazon-scraper-m2h2defbh-engrmumtazali0112s-projects.vercel.app",
+        source: "https://github.com/engrmumtazali0112/python-script-amazon-scraper"
+    },
+
+
+    {
+        imgsrc: p1blogwebsite,
+        title: "Modern Blog Website",
+        text: "A dynamic, full-stack blog website built with Node.js, Express, and MongoDB. Key Features include:\n\n- User Authentication: Secure register/login functionality\n- CRUD Operations: Create, read, update, and delete blog posts\n- Image Upload: Upload images for blog posts\n- Responsive Design: Optimized for mobile and desktop\n- Server-side Rendering: Powered by EJS templates\n- RESTful API Architecture: Clean and efficient API\n- Admin Dashboard: Manage users and posts easily",
+        view: "https://github.com/engrmumtazali0112/Digital_Empowerment_Pakistan_Internships/tree/main/blog-website-node.js-express-mongodb",
+        source: "https://github.com/engrmumtazali0112/Digital_Empowerment_Pakistan_Internships/tree/main/blog-website-node.js-express-mongodb"
+    },
+    
+    
+    
+    
+
+    {
+        imgsrc: p4handgesture,
+        title: "Hand Gesture Recognition",
+        text: "The Hand Gesture Recognition project utilizes computer vision and gesture detection to create an interactive drawing application. This application uses a webcam to detect hand movements and allows users to draw on the screen with their hands. Built with Python, it employs OpenCV, Mediapipe, and NumPy for real-time hand tracking and gesture analysis.",
+        view: "https://github.com/engrmumtazali0112/Code_Clause_Internship_Artificial_Intelligence/tree/main/Gesture%20Recognition%20System%20%7C%20AI-Powered%20Hand%20Gesture%20Detection%20with%20CNN%20and%20OpenCV",
+        source: "https://github.com/engrmumtazali0112/Code_Clause_Internship_Artificial_Intelligence/tree/main/Gesture%20Recognition%20System%20%7C%20AI-Powered%20Hand%20Gesture%20Detection%20with%20CNN%20and%20OpenCV"
+    },
+    
+
+    {
+        imgsrc: p5objdetection,
+        title: "Real-Time Object Detection",
+        text: "A real-time object detection system built with Python and OpenCV. The system detects and classifies multiple objects through a webcam feed or images, using the SSD MobileNet model pre-trained on the COCO dataset. It can identify 80 different object classes with high accuracy.",
+        view: "https://github.com/engrmumtazali0112/Code_Clause_Internship_Artificial_Intelligence/tree/main/simple-real-time-object-detection",
+        source: "https://github.com/engrmumtazali0112/Code_Clause_Internship_Artificial_Intelligence/tree/main/simple-real-time-object-detection"
     },
 
     {
-        imgsrc: pro4,
-        title: "Car Rent Managment system",
-        text: "This is C++ based Car rental management system...",
-        view: "https://github.com/Abuzar-Khan2023/Car-Rental-System",
-        sourse: "https://github.com/Abuzar-Khan2023/Car-Rental-System"
+        imgsrc: p6PersonalityPredictionSystem,
+        title: "Personality Prediction System via CV Analysis",
+        text: "An AI-powered system that analyzes resumes to predict personality traits, helping recruiters make data-driven hiring decisions. The system uses Natural Language Processing (NLP) and Machine Learning to extract information from CVs and generate personality insights.",
+        view: "https://github.com/engrmumtazali0112/Code_Clause_Internship_Artificial_Intelligence/tree/main/Personality-Prediction-System-via-CV-Analysis",
+        source: "https://github.com/engrmumtazali0112/Code_Clause_Internship_Artificial_Intelligence/tree/main/Personality-Prediction-System-via-CV-Analysis"
     },
 
     {
-        imgsrc: pro5,
-        title: "HRC-AJK Website Incomp...",
-        text: "A web for HRC-KP this is under development...",
-        view: "https://abuzar-khan2023.github.io/HRC-AJK/",
-        sourse: "https://github.com/Abuzar-Khan2023/HRC-AJK",
+        imgsrc: p7RubustMultipleChoisequiz, 
+        title: "Robust Multiple-Choice Quiz Game",
+        text: "A dynamic and interactive multiple-choice quiz game built for a robust user experience. The game challenges users with a variety of questions and provides real-time feedback on their answers.",
+        view: "https://github.com/engrmumtazali0112/EcodeCamp_Internships/tree/main/Robust-Multiple-Choice-Quiz-Game",
+        source: "https://github.com/engrmumtazali0112/EcodeCamp_Internships/tree/main/Robust-Multiple-Choice-Quiz-Game"
     },
+
+    {
+        imgsrc: p8HangmanAdventureDjango,
+        title: "Hangman Adventure Django",
+        text: "A responsive Hangman game built with Django, developed as part of the Code Alpha internship program. Code Alpha is a dynamic and innovative company that provides internship opportunities for aspiring developers and tech enthusiasts. This Hangman Adventure game demonstrates skills in web development, responsive design, and game logic implementation.",
+        view: "https://github.com/engrmumtazali0112/code_alpha_hangman_game",
+        source: "https://github.com/engrmumtazali0112/code_alpha_hangman_game"
+    },
+
+    {
+        imgsrc: p9WorldBankPopulationData,
+        title: "Distribution Visualization Project",
+        text: "This project focuses on analyzing and visualizing global population distribution and the distribution of countries by region using bar charts. The analysis leverages the World Bank Population Dataset to provide insights into global demographics and country classifications.",
+        view: "https://github.com/engrmumtazali0112/data-analysis-projects/tree/main/Distribution-Visualization",
+        source: "https://github.com/engrmumtazali0112/data-analysis-projects/tree/main/Distribution-Visualization"
+    },
+
+    {
+        imgsrc: p10TitanicDatasetAnalysisReportimages,
+        title: "Titanic Survival Prediction",
+        text: "Analyze the Titanic dataset to predict passenger survival outcomes based on various features. This project explores relationships between variables such as survival rate, gender, class, fare, and age. A logistic regression model is built to predict survival outcomes.",
+        view: "https://github.com/engrmumtazali0112/data-analysis-projects/tree/main/Titanic-Survival-Prediction",
+        source: "https://github.com/engrmumtazali0112/data-analysis-projects/tree/main/Titanic-Survival-Prediction"
+    },
+
+    {
+        imgsrc: p2ecommerce, 
+        title: "Watch World - E-Commerce Website",
+        text: "Watch World is an E-commerce platform where customers can explore and purchase a wide range of premium watches with free delivery within their city.",
+        view: "https://github.com/engrmumtazali0112/E-Commerce-Website-using-MERN-Stack",
+        source: "https://github.com/engrmumtazali0112/E-Commerce-Website-using-MERN-Stack"
+    },
+    
+
+    {
+        imgsrc: p3studyhub,
+        title: "StudyBud: Interactive Learning Platform",
+        text: "A dynamic web application designed for interactive learning and collaboration. Built with Django, this platform offers secure user authentication and a range of features for managing study rooms and engaging with others in real-time.",
+        view: "https://github.com/engrmumtazali0112/Digital_Empowerment_Pakistan_Internships/tree/main/real-time-chat",
+        source: "https://github.com/engrmumtazali0112/Digital_Empowerment_Pakistan_Internships/tree/main/real-time-chat"
+    },
+
+    {
+        imgsrc: p11ModernMusicPlayer,
+        title: "Modern Music Player",
+        text: "A sleek and modern music player built using Python and Tkinter. The application provides an intuitive and user-friendly interface, offering essential features for managing and enjoying your music collection Basic playback controls (play, pause, stop)",
+        view: "https://github.com/engrmumtazali0112/Codex-Cue_Internship/tree/main/Python-music-app",
+        source: "https://github.com/engrmumtazali0112/Codex-Cue_Internship/tree/main/Python-music-app"
+    },
+
+    {
+        imgsrc: p12DocumentPlagiarismDetector,
+        title: "Document Plagiarism Detector",
+        text: "A robust Flask-based web application that detects plagiarism and AI-generated content across PDF and DOCX documents using advanced NLP techniques and OpenAI's API.",
+        view: "https://github.com/engrmumtazali0112/Codex-Cue_Internship/tree/main/plagiarism-detection-python",
+        source: "https://,github.com/engrmumtazali0112/Codex-Cue_Internship/tree/main/plagiarism-detection-python"
+    },
+
+    {
+        imgsrc: p13VoiceAssistan,
+        title: "Voice Assistant",
+        text: "A voice-controlled personal assistant built with Python, using libraries like Speech Recognition and gTTS.",
+        view: "https://github.com/engrmumtazali0112/Codex-Cue_Internship/tree/main/voice-chat-assistant",
+        source: "https://github.com/engrmumtazali0112/Codex-Cue_Internship/tree/main/voice-chat-assistant"
+    }
+
+    
+
+
+    
+    
 ]
 
 export default ProjectCardData;  
